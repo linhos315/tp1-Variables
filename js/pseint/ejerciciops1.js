91609fdbb8785d3e94be334644d0de7cbf61878d
GalarzeCarlos <carlos.e.galarze@gmail.com>
2025-06-12 15:46:57 -0300

    se realiza el ejercicio 1 de pseint

---
+++ js/pseint/ejerciciops1.js
@@ -0,0 +1,8 @@
+//Realizar una suma de dos números:  
+// Crear un algoritmo que permita al usuario ingresar dos números, realizar la suma de ambos y mostrar el resultado.
+
+const num1 = (parseInt(prompt("Ingrese el primer numero a sumar")));
+const num2 = (parseInt(prompt("Ingrese el segundo numero a sumar")));
+const suma = (num1 + num2);
+
+document.writeln("El resultado de la suma es: "+suma);
