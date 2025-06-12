ef0976b31d6ecad5fa6599aa194c79ffe6f5359d
GalarzeCarlos <carlos.e.galarze@gmail.com>
2025-06-12 15:59:05 -0300

    se resuelve ejercicio 3 de pseint

---
+++ js/pseint/ejerciciops3.js
@@ -0,0 +1,7 @@
+//Multiplicar un número por 10:  
+// Crear un algoritmo que permita al usuario ingresar un número y luego mostrar el resultado de multiplicarlo por 10.
+
+const number = (parseInt(prompt("Ingrese un numero")));
+const multiplicacionx10 = (number * 10)
+
+document.writeln("El resultado de la multiplicacion en 10 es: "+ multiplicacionx10);
