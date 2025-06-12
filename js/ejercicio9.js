0b6565c441ea2f13968e65fc8473ae7294806ca0
GalarzeCarlos <carlos.e.galarze@gmail.com>
2025-06-12 14:35:15 -0300

    se resuelve el ejercicio 9

---
+++ js/ejercicio9.js
@@ -0,0 +1,125 @@
+/*
+9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+Ejemplo:
+
+
+
+input: Hola mundo
+Output: oauo
+
+*/
+
+const frase = prompt(
+  "Ingrese una palabra de hasta 10 caracteres"
+).toLowerCase();
+
+if (
+  frase.charAt(0) === "a" ||
+  frase.charAt(0) === "e" ||
+  frase.charAt(0) === "i" ||
+  frase.charAt(0) === "o" ||
+  frase.charAt(0) === "u"
+) {
+  document.writeln(frase.charAt(0));
+}
+
+if (
+  frase.charAt(1) === "a" ||
+  frase.charAt(1) === "e" ||
+  frase.charAt(1) === "i" ||
+  frase.charAt(1) === "o" ||
+  frase.charAt(1) === "u"
+) {
+  document.writeln(frase.charAt(1));
+}
+
+if (
+  frase.charAt(2) === "a" ||
+  frase.charAt(2) === "e" ||
+  frase.charAt(2) === "i" ||
+  frase.charAt(2) === "o" ||
+  frase.charAt(2) === "u"
+) {
+  document.writeln(frase.charAt(2));
+}
+
+if (
+  frase.charAt(3) === "a" ||
+  frase.charAt(3) === "e" ||
+  frase.charAt(3) === "i" ||
+  frase.charAt(3) === "o" ||
+  frase.charAt(3) === "u"
+) {
+  document.writeln(frase.charAt(3));
+}
+
+if (
+  frase.charAt(4) === "a" ||
+  frase.charAt(4) === "e" ||
+  frase.charAt(4) === "i" ||
+  frase.charAt(4) === "o" ||
+  frase.charAt(4) === "u"
+) {
+  document.writeln(frase.charAt(4));
+}
+
+if (
+  frase.charAt(5) === "a" ||
+  frase.charAt(5) === "e" ||
+  frase.charAt(5) === "i" ||
+  frase.charAt(5) === "o" ||
+  frase.charAt(5) === "u"
+) {
+  document.writeln(frase.charAt(5));
+}
+
+if (
+  frase.charAt(6) === "a" ||
+  frase.charAt(6) === "e" ||
+  frase.charAt(6) === "i" ||
+  frase.charAt(6) === "o" ||
+  frase.charAt(6) === "u"
+) {
+  document.writeln(frase.charAt(6));
+}
+
+if (
+  frase.charAt(7) === "a" ||
+  frase.charAt(7) === "e" ||
+  frase.charAt(7) === "i" ||
+  frase.charAt(7) === "o" ||
+  frase.charAt(7) === "u"
+) {
+  document.writeln(frase.charAt(7));
+}
+
+if (
+  frase.charAt(8) === "a" ||
+  frase.charAt(8) === "e" ||
+  frase.charAt(8) === "i" ||
+  frase.charAt(8) === "o" ||
+  frase.charAt(8) === "u"
+) {
+  document.writeln(frase.charAt(8));
+}
+
+if (
+  frase.charAt(9) === "a" ||
+  frase.charAt(9) === "e" ||
+  frase.charAt(9) === "i" ||
+  frase.charAt(9) === "o" ||
+  frase.charAt(9) === "u"
+) {
+  document.writeln(frase.charAt(9));
+}
+
+if (
+  frase.charAt(10) === "a" ||
+  frase.charAt(10) === "e" ||
+  frase.charAt(10) === "i" ||
+  frase.charAt(10) === "o" ||
+  frase.charAt(10) === "u"
+) {
+  document.writeln(frase.charAt(10));
+}
